9413ce78c2972857ae347bfd6bcddae9bb7b06c2
Renzo Solis <rensoliscastillo@gmail.com>
2018-10-06 15:28:42 -0500

    Added bulma functionality

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const routes = require('./routes/routes');
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'src/public')));
 app.use('/', routes);
 
 app.set('port', process.env.PORT || 3000);
