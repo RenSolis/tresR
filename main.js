583fc6fa20ad717373c9a6bd8da99782d7c9db3e
Renzo Solis <rensoliscastillo@gmail.com>
2018-10-12 21:40:56 -0500

    New orden

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+// Modules
 const path = require('path');
+const ejs = require('ejs-mate');
 const routes = require('./routes/routes');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'src/public')));
+//  Middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', ejs);
+app.set('views', path.join(__dirname, 'views'));
+
+//  Routes
 app.use('/', routes);
 
+//  Create server
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
--- routes/routes.js
+++ routes/routes.js
@@ -7,47 +7,51 @@ function route(route) {
 }
 
 router.get('/', (req, res) => {
-    res.sendFile(route('../src/views/Congreso.html'));
+    res.sendFile(route('../views/Congreso.html'));
 });
 
 router.get('/login', (req, res) => {
-    res.sendFile(route('../src/views/Iniciar_Sesion.html'));
+    res.sendFile(route('../views/Iniciar_Sesion.html'));
 });
 
 router.get('/materials', (req, res) => {
-    res.sendFile(route('../src/views/Materiales.html'));
+    res.sendFile(route('../views/Materiales.html'));
 });
 
 router.get('/materials/paperboard', (req, res) => {
-    res.sendFile(route('../src/views/MaterialCarton.html'));
+    res.sendFile(route('../views/MaterialCarton.html'));
 });
 
 router.get('/materials/electronic', (req, res) => {
-    res.sendFile(route('../src/views/MaterialElectronico.html'));
+    res.sendFile(route('../views/MaterialElectronico.html'));
 });
 
 router.get('/materials/paper', (req, res) => {
-    res.sendFile(route('../src/views/MaterialPapel.html'));
+    res.sendFile(route('../views/MaterialPapel.html'));
 });
 
 router.get('/materials/plastic', (req, res) => {
-    res.sendFile(route('../src/views/MaterialPlastico.html'));
+    res.sendFile(route('../views/MaterialPlastico.html'));
 });
 
 router.get('/materials/glass', (req, res) => {
-    res.sendFile(route('../src/views/MaterialVidrio.html'));
+    res.sendFile(route('../views/MaterialVidrio.html'));
 });
 
 router.get('/statistics', (req, res) => {
-    res.sendFile(route('../src/views/Estadisticas.html'));
+    res.sendFile(route('../views/Estadisticas.html'));
 });
 
 router.get('/qr', (req, res) => {
-    res.sendFile(route('../src/views/QR.html'));
+    res.sendFile(route('../views/QR.html'));
+});
+
+router.get('/signup', (req, res) => {
+    res.sendFile(route('../views/Registro.html'));
 });
 
 router.get('/claims', (req, res) => {
-    res.sendFile(route('../src/views/Reclamos.html'));
+    res.sendFile(route('../views/Reclamos.html'));
 });
 
 module.exports = router;
